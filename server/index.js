914e598830bde0dc29ab4d2830e7e56359a76849
tanmayanandx <54024121+tanmayanandx@users.noreply.github.com>
2022-08-09 06:36:15 +0530

    dotenv  update

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -14,9 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://memories_project:memories_project123@cluster0.kx63b.mongodb.net/?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://memories_project:memories_project123@cluster0.kx63b.mongodb.net/?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.CONNECTION_URL  || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
