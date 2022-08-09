351edb71e8bf26346b82e2c6593d52f7a0e0ad44
tanmayanandx <54024121+tanmayanandx@users.noreply.github.com>
2022-08-09 20:19:18 +0530

    wqr

--- server/index.js
+++ server/index.js
@@ -17,11 +17,16 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //const CONNECTION_URL = 'mongodb+srv://memories_project:memories_project123@cluster0.kx63b.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT  || 5000;
 
-const PORT = process.env.CONNECTION_URL  || 5000;
+//const PORT = process.env.CONNECTION_URL  || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
 
 //mongoose.set('useFindAndModify', false);
